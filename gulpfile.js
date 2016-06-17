f98c9bf53999fcffb21099659f47e40ce6b06a1a
Belén Albeza <belen.albeza@gmail.com>
2016-06-17 14:47:37 +0100

    fix bug in building system

--- gulpfile.js
+++ gulpfile.js
@@ -7,57 +7,55 @@ var browserify = require('browserify');
 var watchify = require('watchify');
 var source = require('vinyl-source-stream');
 
-var connect = require('gulp-connect');
-var livereload = require('gulp-livereload');
+var browserSync = require('browser-sync').create();
+var del = require('del');
+var merge = require('merge-stream');
+
+
+// IMPORTANT
+// edit gulp.config.json and customize there your deployment settings
 var rsync = require('gulp-rsync');
-
-// load up config file
-var config  = require('./gulp.config.json');
-
-//
-// browserify and JS
-//
-
-var bundler = browserify('./app/js/main.js');
-
-function bundle() {
-  return bundler.bundle()
-    .on('error', gutil.log.bind(gutil, 'Browserify error'))
-    .pipe(source('bundle.js'))
-    .pipe(gulp.dest('.tmp/js'))
-    .pipe(livereload());
+var config = {};
+try {
+  config = require('./gulp.config.json');
 }
+catch (e) {
+  console.warn('Edit or create gulp.config.json to customize your deployment' +
+    ' settings.');
+}
+
+var ghpages = require('gulp-gh-pages');
+
+
+
+//
+// browserify and js
+//
+
+var bundler = browserify([
+  './app/js/main.js'
+]);
+
+var bundle = function ()  {
+  return bundler
+    .bundle()
+    .on('error', gutil.log)
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest('.tmp/js/'))
+    .pipe(browserSync.stream({once: true}));
+};
+
 
 gulp.task('browserify', bundle);
 
-// 3rd party libs that don't play nice with browserify :(
+// 3rd party libs that don't play nice with browserify
 gulp.task('libs', function () {
-  gulp.src(['phaser.min.js', 'phaser.map'], {cwd: 'node_modules/phaser/dist/'})
-    .pipe(gulp.dest('.tmp/js/lib/'));
+  var dir = './node_modules/phaser/dist/';
+  return gulp.src(['phaser.min.js', 'phaser.map'], { cwd: dir, base: dir})
+    .pipe(gulp.dest('./.tmp/js/lib/'));
 });
 
 gulp.task('js', ['browserify', 'libs']);
-
-//
-// web server
-//
-
-gulp.task('connect', function () {
-  connect.server({
-    root: ['app', '.tmp']
-  });
-});
-
-//
-// watch
-//
-
-gulp.task('watch', ['connect'], function () {
-  livereload.listen();
-
-  bundler = watchify(bundler, watchify.args);
-  bundler.on('update', bundle);
-});
 
 //
 // build and deploy
@@ -65,21 +63,27 @@ gulp.task('watch', ['connect'], function () {
 
 gulp.task('build', ['js']);
 
-gulp.task('copy', function () {
-  gulp.src([
-    'index.html', 'css/*.css', 'images/**/*', 'fonts/**/*', 'audio/**/*'
-  ], {cwd: 'app', base: 'app'})
+gulp.task('dist', ['build'], function () {
+  var rawFiles = gulp.src([
+    'index.html', 'raw.html',
+    '*.css',
+    'images/**/*', 'fonts/**/*', 'audio/**/*'
+  ], { cwd: './app', base: './app' })
     .pipe(gulp.dest('./dist/'));
 
-
-  gulp.src(['js/**/*'], {cwd: '.tmp', base: '.tmp'})
+  var builtFiles = gulp.src(['js/**/*'], { cwd: '.tmp', base: '.tmp' })
     .pipe(gulp.dest('./dist/'));
+
+  return merge(rawFiles, builtFiles);
 });
 
-gulp.task('dist', ['build', 'copy']);
+gulp.task('clean', function () {
+  return del(['.tmp', 'dist', '.publish']);
+});
 
-gulp.task('deploy', ['dist'], function () {
-  gulp.src('dist')
+
+gulp.task('deploy:rsync', ['dist'], function () {
+  return gulp.src('dist')
     .pipe(rsync({
       root: 'dist',
       username: config.deploy.user,
@@ -92,14 +96,33 @@ gulp.task('deploy', ['dist'], function () {
     }));
 });
 
+gulp.task('deploy:ghpages', ['dist'], function () {
+  return gulp.src('dist/**/*')
+    .pipe(ghpages());
+});
+
+
+
+
 //
 // dev tasks
 //
 
-gulp.task('server', ['build', 'watch']);
+gulp.task('watch', function () {
+  bundler = watchify(bundler, watchify.args);
+  bundler.on('update', bundle);
+});
+
+gulp.task('run', ['watch', 'build'], function () {
+  browserSync.init({
+    server: ['app', '.tmp']
+  });
+
+  gulp.watch('app/**/*.{html,css}').on('change', browserSync.reload);
+});
 
 //
 // default task
 //
 
-gulp.task('default', ['build', 'connect']);
+gulp.task('default', ['dist']);
